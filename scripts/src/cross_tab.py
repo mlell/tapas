1fac45b198a7233282bd52dea400fe99c918546d
Moritz Lell <moritz.lell@uni-potsdam.de>
2017-04-03 14:24:55 +0200

    cross_tab: ignore empty lines
    This fixes an issue with cross_tab if it is provided empty lines: Before
    it generated empty values in the output table. As newlines are sometimes
    inadvertently left at the end of input files, this can be a nuissance,
    which may cause errors downstream

--- scripts/src/cross_tab.py
+++ scripts/src/cross_tab.py
@@ -8,6 +8,10 @@ from itertools import product
 helpText=r"""
 Usage: cross_tab.py [options] FILES...
 
+Print all combinations of the input tables' rows. Empty lines or lines
+containing only whitespace are ignored, unless explicitly allowed 
+(see parameters.)
+
 Options:
     --skip INT  [default: 0] 
                 Skip this number of lines from the top of each 
@@ -19,6 +23,40 @@ Options:
     --sep CHAR  [Default: \t] 
                 A character which separates the contents
                 of the input files in the output columns
+    --respect-empty
+                Do not remove empty lines or lines containing only whitespace
+                in the input files
+
+Example:
+
+    ── tab1 ──
+    a   b
+    1   3
+    2   4
+    ──────────
+
+    ── tab2 ──
+    X   
+    a   
+    b   
+
+    ──────────
+    
+    Call: cross_tab --head 1 
+
+    yields: 
+
+    ──────────
+    a   b   X
+    1   3   a
+    1   3   b
+    2   4   a
+    2   4   b
+    ──────────
+
+    Note how the empty lines in tab2 was ignored. Use --respect-empty to 
+    change this behaviour.
+
 """
 
 
@@ -27,6 +65,7 @@ def main(argv):
     skip = int(args["--skip"])
     header= int(args["--head"])
     sep = "\t" if args["--sep"] == r"\t" else args["--sep"]
+    respect = args["--respect-empty"]
 
     # Read in every file. input_lines will be of the following
     # format: [[file1_line1, file1_line2, ... ],
@@ -35,6 +74,9 @@ def main(argv):
     for file in args["FILES"]: 
         with open(file,"rt") as fd:
             lines = [l.rstrip("\n") for l in fd]
+            if not respect:
+                # Ignore empty lines
+                lines = [l for l in lines if l.strip() != ""]
             input_lines.append(lines[skip:])
     
     if header > 0:
